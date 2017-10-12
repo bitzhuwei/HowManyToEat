500451ff3ac6657fa6349930d95c6cb931d0aff3
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-10-13 01:55:15 +0800

    new dish, new ingredient

--- HowManyToEat/FormNewIngrendient.Designer.cs
+++ HowManyToEat/FormNewIngrendient.Designer.cs
@@ -39,16 +39,18 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnOK
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOK.Location = new System.Drawing.Point(178, 131);
+            this.btnOK.Location = new System.Drawing.Point(178, 165);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(109, 23);
             this.btnOK.TabIndex = 13;
-            this.btnOK.Text = "确定";
+            this.btnOK.Text = "保存";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
@@ -56,7 +58,7 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(293, 131);
+            this.btnCancel.Location = new System.Drawing.Point(293, 165);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(109, 23);
             this.btnCancel.TabIndex = 14;
@@ -87,7 +89,7 @@
             this.textBox2.Font = new System.Drawing.Font("宋体", 14F);
             this.textBox2.Location = new System.Drawing.Point(123, 12);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(271, 29);
+            this.textBox2.Size = new System.Drawing.Size(279, 29);
             this.textBox2.TabIndex = 16;
             this.textBox2.Text = "青椒";
             // 
@@ -105,7 +107,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 14F);
-            this.label3.Location = new System.Drawing.Point(13, 50);
+            this.label3.Location = new System.Drawing.Point(13, 85);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 19);
             this.label3.TabIndex = 15;
@@ -114,9 +116,9 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("宋体", 14F);
-            this.textBox3.Location = new System.Drawing.Point(123, 47);
+            this.textBox3.Location = new System.Drawing.Point(123, 82);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(271, 29);
+            this.textBox3.Size = new System.Drawing.Size(279, 29);
             this.textBox3.TabIndex = 16;
             this.textBox3.Text = "斤";
             // 
@@ -124,7 +126,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 14F);
-            this.label4.Location = new System.Drawing.Point(13, 85);
+            this.label4.Location = new System.Drawing.Point(13, 120);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(104, 19);
             this.label4.TabIndex = 15;
@@ -133,22 +135,41 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("宋体", 14F);
-            this.textBox4.Location = new System.Drawing.Point(123, 82);
+            this.textBox4.Location = new System.Drawing.Point(123, 117);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(271, 29);
+            this.textBox4.Size = new System.Drawing.Size(279, 29);
             this.textBox4.TabIndex = 16;
             this.textBox4.Text = "0";
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(17, 131);
+            this.button1.Location = new System.Drawing.Point(12, 165);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 23);
             this.button1.TabIndex = 13;
-            this.button1.Text = "确定并继续录入";
+            this.button1.Text = "保存并继续录入";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14F);
+            this.label5.Location = new System.Drawing.Point(13, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 19);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "食材类别：";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("宋体", 14F);
+            this.textBox5.Location = new System.Drawing.Point(123, 47);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(279, 29);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.Text = "蔬菜";
             // 
             // FormNewIngrendient
             // 
@@ -156,11 +177,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(414, 166);
+            this.ClientSize = new System.Drawing.Size(414, 200);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
@@ -188,5 +211,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
